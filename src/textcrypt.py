170df1823fb6f82a74c055241514447864de3e5f
Vladimir Zurita <vladzur-HP-430-Notebook-PC>
2013-05-03 11:14:02 -0400

    First comit

---
+++ src/textcrypt.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+# -*- Mode: Python; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*- 
+#
+# main.py
+# Copyright (C) 2013 Vladimir Zurita <vladzur@gmail.com>
+# 
+# pycrypt-text is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# pycrypt-text is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License along
+# with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from gi.repository import Gtk, GdkPixbuf, Gdk
+import os, sys, base64
+
+
+#Comment the first line and uncomment the second before installing
+#or making the tarball (alternatively, use project variables)
+UI_FILE = "src/textcrypt.ui"
+#UI_FILE = "/usr/local/share/textcrypt/ui/textcrypt.ui"
+
+
+class GUI:
+	def __init__(self):
+
+		self.builder = Gtk.Builder()
+		self.builder.add_from_file(UI_FILE)
+		self.builder.connect_signals(self)
+
+		window = self.builder.get_object('window')	
+
+		window.show_all()
+		
+
+	def open_clicked(window, self):
+		filechooser = window.builder.get_object('filechooserdialog1')
+		filechooser.show_all()
+
+	def on_button1_cancel_clicked(window, self):
+		self.hide()
+	def on_button1_ok_clicked(window, self):
+		textarea = window.builder.get_object('textentry')
+		filename = self.get_filename()
+		f = open(filename, 'r')
+		content = f.read()
+		text_buffer = Gtk.TextBuffer()
+		text_buffer.set_text(content)
+		textarea.set_buffer(text_buffer)
+		self.hide()
+
+	def on_menubase64_encode_activate(window, self):
+		textbuffer = Gtk.TextBuffer()
+		textbuffer = self.get_buffer()
+		texto = textbuffer.get_text(textbuffer.get_start_iter(), textbuffer.get_end_iter(), True)
+		texto_encoded = base64.b64encode(texto)
+		textbuffer.set_text(texto_encoded)
+		self.set_buffer(textbuffer)
+
+	def on_menubase64_decode_activate(window, self):
+		textbuffer = Gtk.TextBuffer()
+		textbuffer = self.get_buffer()
+		texto = textbuffer.get_text(textbuffer.get_start_iter(), textbuffer.get_end_iter(), True)
+		texto_encoded = base64.b64decode(texto)
+		textbuffer.set_text(texto_encoded)
+		self.set_buffer(textbuffer)
+		
+	def destroy(window, self):
+		Gtk.main_quit()
+
+	def quit_clicked(window, self):
+		Gtk.main_quit()
+
+def main():
+	app = GUI()
+	Gtk.main()
+		
+if __name__ == "__main__":
+	sys.exit(main())
+
